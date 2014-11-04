a6c83475c8a3c2098fbf976a6f91352cb9890497
Anton K. Andersson <jag@antonkandersson.se>
2014-11-04 14:14:47 +0100

    scraping images, saving to json. triple data sets, fix.

--- lab1/scraper/scraper.js
+++ lab1/scraper/scraper.js
@@ -3,21 +3,18 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 var url = 'http://www.svtplay.se/';
-var jadeVars = {
+var scraped = {
 	site: url,
-	img: {},
-	title: ''
+	imgsrc: [],
+	title: '',
+	date_scraped: Date()
 }
 
 // not yet implemented
-var Nightmare = require('nightmare');
-var user = require('./user/user.js');
-var fb = require('./lib/fblogin/fblogin.js');
-
-
-module.exports = function (app) {
-	console.log('app in scraper.js');
-	app.get('/scraper', function (req, res) {
+// var Nightmare = require('nightmare');
+// var user = require('./user/user.js');
+// var fb = require('./lib/fblogin/fblogin.js');
+// put inside of request
 		// login to facebook
 		// new Nightmare()
 		// .use(fb.login(user.email, user.pw))
@@ -26,35 +23,33 @@ module.exports = function (app) {
 		// request the user's profile after login
 		// var url = user.fbprofile + '/photos';
 
+module.exports = function (app) {
+	console.log('app in scraper.js');
+	app.get('/scraper', function (req, res) {
 
 		request(url, function(error, response, html){
 			if (!error) {
 				var loader = cheerio.load(html);
-				var name, profilepic;
-				var json = {
-					name: '',
-					profilepic: ''
-				};
 
 				loader('.play_videolist-element__thumbnail-image').filter(function () {
 					var data = loader(this);
-					// console.log(data);
-					jadeVars.imgsrc = data[0].attribs.src;
-					console.log(jadeVars.imgsrc);
+					Object.keys(data[0].attribs).forEach(function (element) {
+						console.log(data[0].attribs[element]);
+						scraped.imgsrc.push(data[0].attribs['src']);
+					});
 				});
 			}
+			fs.writeFile('scraped_data/scraped.json', JSON.stringify(scraped, null, 4), function(err){
+				if (err) {
+					throw err;
+				}
+				console.log('Scraper saved to JSON: scraped.json');
+			})
 		});
-		fs.writeFile('scraped_data/scraped.json', JSON.stringify(jadeVars, null, 4), function(err){
-			if (err) {
-				throw err;
-			}
-			console.log('Scraper saved to JSON: scraped.json');
-    })
-
 
 		res.render('scraper', {
-			url: jadeVars.site,
-			images: jadeVars.imgsrc
+			url: scraped.site,
+			images: scraped.imgsrc
 		});
 	});
 }
