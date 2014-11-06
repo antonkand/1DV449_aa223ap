8dff4feaffa5c8d26c362c717bd008f54e799d62
Anton K. Andersson <jag@antonkandersson.se>
2014-11-06 11:19:23 +0100

    selectors left

--- lab1/scraper/coursepress/coursepress.js
+++ lab1/scraper/coursepress/coursepress.js
@@ -1,36 +1,108 @@
 'use strict';
-// @loadedCheerio: a cheerio object with loaded dom
-// @scrapeobject: a JS object to write to
-function getCourses (loadedCheerio, scrapeobject) {
-			if (!loadedCheerio || !scrapeobject) {
-				console.log('cheerio.getCourses: missing params.');
-			}
-			loadedCheerio('.item-title a').filter(function () {
-			var data = loadedCheerio(this);
-			data.each(function () {
-				var course = {
-					coursename: data.text(),
-					url: data[0].attribs['href']
-				};
-			scrapeobject.courses.push(course);
-			scrapeobject.scraped_courses = scrapeobject.courses.length;
-			});
-		});
-}
-
-var coursepress = {};
-
-coursepress.options = {
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var options = {
 	url: 'http://coursepress.lnu.se/kurser',
 	headers: {
 		'User-Agent': 'Webbteknik II som skrapar'
 	}
 };
+var filename = 'coursepress.json';
+var jsonlink = '/coursepress';
+var json = {
+	site: options.url,
+	date_scraped: new Date(),
+	courses: [],
+	scraped_courses: 0
+};
 
-coursepress.cheerio = {};
-coursepress.cheerio.getCourses = getCourses;
+// @loadedCheerio: a cheerio object with loaded dom
+// @scrapeobject: a JS object to write to
+function scrapeCourses (loadedCheerio, callback) {
+			if (!loadedCheerio) {
+				console.log('cheerio.getCourses: missing params.');
+			// load all the urls and titles,
+			// push to course {}
+			}
+			loadedCheerio('.item-title a').filter(function () {
+			var data = loadedCheerio(this);
+			var course = {
+				coursename: '',
+				url: '',
+				description: '',
+				first_blogpost: {
+					title: '',
+					author: '',
+					date: '',
+					post_body: ''
+				}
+			};
+			data.each(function () {
+				course.coursename = data.text();
+				course.url = data[0].attribs['href'];
+				// make new request for each subpage,
+				// push to course {}
+				request(course.url, function (error, response, html) {
+					if (!error && response.statusCode === 200) {
+						var loader = cheerio.load(html);
+						// short description of course
+						loader('div.entry-content').filter(function () {
+							var courseDescription = loader(this);
+							// TODO: Fix course description
+							// console.log(courseDescription.first().text());
+							course.description = courseDescription.first().text();
+						});
+						// first blog post
+						loader('header.entry-header').filter(function () {
+							var blogPost = loader(this);
+							// console.log(blogPost.children().first().text()); // h1 with title
+							console.log(blogPost.children().first()); // strong with author
+							course.first_blogpost.title = blogPost.children().first().text();
+							// course.first_blogpost.author = blogPost.children().second().text();
+						});
 
-exports.options = coursepress.options;
-exports.cheerio = coursepress.cheerio;
-exports.filename = 'coursepress.json';
-exports.jsonlink = '/coursepress';
+					callback(course);
+					}
+				});
+			});
+		});
+}
+
+// writes the scraped data to file
+function saveDataToJSON () {
+		fs.writeFile('public/scraped_data/' + filename, JSON.stringify(json, null, 4), function(err){
+			if (err) {
+					throw err;
+			}
+			console.log('Scraper saved to JSON: ' + filename);
+		});
+}
+
+// used as callback when scraping
+function pushCourse (course) {
+	// console.log('pushCourse @course:');
+	// console.log(course);
+	json.courses.push(course);
+	json.scraped_courses = json.courses.length;
+}
+
+
+function scrape () {
+	request(options, function(error, response, html){
+			if (!error && response.statusCode === 200) {
+				var loader = cheerio.load(html);
+				scrapeCourses(loader, pushCourse);
+			}
+		});
+		setTimeout(saveDataToJSON, 15000);
+}
+
+var coursepress = {
+	scrape: scrape,
+	filename: filename,
+	jsonlink: jsonlink,
+	url: options.url
+};
+
+module.exports = coursepress;
--- lab1/scraper/scraper.js
+++ lab1/scraper/scraper.js
@@ -1,15 +1,6 @@
 'use strict';
-var request = require('request');
-var cheerio = require('cheerio');
-var fs = require('fs');
 var chalk = require('chalk');
 var coursepress = require('./coursepress/coursepress.js');
-var scraped = {
-	site: coursepress.options.url,
-	courses: [],
-	scraped_courses: 0,
-	date_scraped: new Date()
-};
 
 // renders a console header used on init
 var displayHeader = function () {
@@ -22,30 +13,12 @@ var timeLimitHasPassed = function () {
 	var now = new Date();
 };
 
-
-// writes the scraped data to file
-var saveDataToJSON = function () {
-		var filename = coursepress.filename;
-		fs.writeFile('public/scraped_data/' + filename, JSON.stringify(scraped, null, 4), function(err){
-			if (err) {
-					throw err;
-			}
-			console.log('Scraper saved to JSON: ' + filename);
-		});
-};
-
 module.exports = function (app) {
 	displayHeader();
 	app.get('/scraper', function (req, res) {
-		request(coursepress.options, function(error, response, html){
-			if (!error) {
-				var loader = cheerio.load(html);
-				coursepress.cheerio.getCourses(loader, scraped);
-				saveDataToJSON();
-			}
-		});
+		coursepress.scrape();
 		res.render('scraper', {
-			url: coursepress.options.url
+			url: coursepress.url
 			// ,
 			// jsonlink: coursepress.jsonlink,
 			// jsonname: coursepress.filename
