0c76baf139acdde237186b189cc647e3c922dba4
Anton K. Andersson <jag@antonkandersson.se>
2014-12-05 11:23:15 +0100

    markers mapped

--- lab3/traffic-report/frontend/src/js/angular/traffic-report/TrafficReportApp.js
+++ lab3/traffic-report/frontend/src/js/angular/traffic-report/TrafficReportApp.js
@@ -19,10 +19,10 @@ function TrafficReportAppController($http, $scope) {
             }
         ]
     };
-    this.map = { center: {latitude: 63, longitude: 17}, zoom: 5, bounds: {}};
+    this.map = {center: {latitude: 63, longitude: 17}, zoom: 5, bounds: {}};
     $http.get('http://localhost:8080/traffic-data')
         .success(function (data, status, headers, config) {
-            data.forEach(function(trafficmessage) {
+            data.forEach(function (trafficmessage) {
                 return that.messages.events.push({
                     priority: trafficmessage.priority,
                     createddate: trafficmessage.createddate,
@@ -35,17 +35,39 @@ function TrafficReportAppController($http, $scope) {
                     subcategory: trafficmessage.subcategory
                 });
             });
-            that.markers = that.messages.events.map(function(marker, index){
-                    return {
-                       id:  'trafficMarker' + (index+1).toString(),
-                       coords: {
-                           latitude: marker.latitude,
-                           longitude: marker.longitude
-                       }
-                    };
+            that.markers = that.messages.events.map(function (marker, index) {
+                var cssClass = '';
+                console.log(marker.priority);
+                switch (marker.priority) {
+                    case 1:
+                        cssClass = 'danger';
+                        break;
+                    case 2:
+                        cssClass = 'warning';
+                        break;
+                    case 3:
+                        cssClass = 'info';
+                        break;
+                    case 4:
+                        cssClass = 'default';
+                        break;
+                    case 5:
+                        cssClass = 'success';
+                        break;
+                }
+                return {
+                    id: 'trafficMarker' + (index + 1).toString(),
+                    title: marker.title,
+                    description: marker.description,
+                    coords: {
+                        latitude: marker.latitude,
+                        longitude: marker.longitude
+                    },
+                    priorityColor: cssClass
+                };
             });
-            console.table(that.messages.events);
-            console.table(that.markers);
+            //console.table(that.messages.events);
+            //console.table(that.markers);
         })
         .error(function (data, status, headers, config) {
             console.log('GET error');
