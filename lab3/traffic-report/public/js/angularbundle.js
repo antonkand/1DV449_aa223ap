c895676c5b193b3d0a3e976a56ea1abfb657e1b0
Anton <anton@Antons-MacBook-Pro.local>
2014-12-08 15:19:33 +0100

    different colors dep on category, infowindows up, date started

--- lab3/traffic-report/public/js/angularbundle.js
+++ lab3/traffic-report/public/js/angularbundle.js
@@ -1,4 +1,6 @@
-'use strict';
+;(function () {
+    'use strict';
+
 angular.module('TrafficReportApp', ['TrafficMenuComponent', 'uiGmapgoogle-maps'])
     .controller('TrafficReportAppController', TrafficReportAppController);
 
@@ -20,8 +22,8 @@ function TrafficReportAppController($http, $scope) {
             }
         ]
     };
-    this.map = {center: {latitude: 63, longitude: 17}, zoom: 5, bounds: {}};
-    this.displayInfo = function (description) {
+    this.map = {center: {latitude: 63, longitude: 17}, zoom: 4, bounds: {}};
+    this.closeWindow = function (description) {
         //if (infoWindow) {
         //    infoWindow.close();
         //}
@@ -29,7 +31,7 @@ function TrafficReportAppController($http, $scope) {
     };
     $http.get('http://localhost:8080/traffic-data')
         .success(function (data, status, headers, config) {
-            data.forEach(function (trafficmessage) {
+          data.forEach(function (trafficmessage) {
                 return that.messages.events.push({
                     priority: trafficmessage.priority,
                     createddate: trafficmessage.createddate,
@@ -42,42 +44,90 @@ function TrafficReportAppController($http, $scope) {
                     subcategory: trafficmessage.subcategory
                 });
             });
-            that.markers = that.messages.events.map(function (marker, index) {
-                var cssClass = '';
-                switch (marker.priority) {
-                    case 1:
-                        cssClass = 'danger';
-                        break;
-                    case 2:
-                        cssClass = 'warning';
-                        break;
-                    case 3:
-                        cssClass = 'info';
-                        break;
-                    case 4:
-                        cssClass = 'default';
-                        break;
-                    case 5:
-                        cssClass = 'success';
-                        break;
-                }
-                return {
-                    id: (index + 12345),
-                    title: marker.title,
-                    //description: marker.description,
-                    latitude: marker.latitude,
-                    longitude: marker.longitude,
-                    labelClass: cssClass,
-                    markerPriorityColor: cssClass,
-                    //display: function () {
-                    //    return that.displayInfo(marker.description);
-                    //}
-                    labelContent: marker.description
-                };
-            });
-            //console.table(that.messages.events);
-            //console.table(that.markers);
-        })
+          that.markers = that.messages.events.map(function (marker, index) {
+              var hexColor = '';
+              var category = '';
+              // TODO make sure returned date is correct + contains dd
+              var unixStringAsDateString = function (unixTimeStampString) {
+                  var hourAsMs = 60*60*1000;
+                  var strippedDate = parseInt(unixTimeStampString) + hourAsMs;
+                  var date = new Date(strippedDate * 1000);
+                  var hours = date.getHours();
+                  var minutes = +'0' + date.getMinutes();
+                  var seconds = +'0' + (date.getMilliseconds()/1000);
+                  console.log(date.getHours());
+                  console.log(date.getMinutes());
+                  console.log((date.getMilliseconds() / 1000));
+                  return hours + ':' + minutes + ':' + seconds;
+              };
+              var timestamp = unixStringAsDateString(marker.createddate.substring(6, marker.createddate.length-7));
+              switch (marker.priority) {
+                  case 1:
+                      hexColor = '#F51329';
+                      break;
+                  case 2:
+                      hexColor = '#F98020';
+                      break;
+                  case 3:
+                      hexColor = '#F9EB18';
+                      break;
+                  case 4:
+                      hexColor = '#85E0F7';
+                      break;
+                  case 5:
+                      hexColor = '#85E0F7';
+                      break;
+              }
+              switch (marker.category) {
+                  case 0:
+                      category = 'Vägtrafik';
+                      break;
+                  case 1:
+                      category = 'Kollektivtrafik';
+                      break;
+                  case 2:
+                      category = 'Planerad störning';
+                      break;
+                  case 3:
+                      category = 'Övrigt';
+                      break;
+              }
+              console.log(google.maps.InfoWindow);
+              return {
+                  id: (index + 12345),
+                  title: marker.title,
+                  latitude: marker.latitude,
+                  longitude: marker.longitude,
+                  description: marker.description,
+                  labelClass: hexColor,
+                  markerPriorityColor: hexColor,
+                  options: { animation: null, visible: true},
+                  labelContent: marker.description,
+                  show: false,
+                  category: category,
+                  categoryNumber: marker.category,
+                  name: marker.name,
+                  zoom: marker.zoom,
+                  date: timestamp,
+                  icon: {
+                      path: google.maps.SymbolPath.CIRCLE,
+                      strokeColor: hexColor,
+                      strokeWeight: 0.8,
+                      fillOpacity: 0.5,
+                      fillColor: hexColor,
+                      scale: 6
+                  },
+                  checkForOpenWindow: function () {
+                      //if (google.maps.InfoWindow) {
+                      //    google.maps.InfoWindow.close();
+                      //}
+                      return function () {
+                        console.log(google.maps.InfoWindow);
+                      };
+                  }
+              };
+          });
+      })
         .error(function (data, status, headers, config) {
             console.log('GET error');
             console.log(data);
@@ -86,8 +136,9 @@ function TrafficReportAppController($http, $scope) {
             console.log(config);
         });
     console.log('TrafficReportAppController initialized.');
-}
-TrafficReportAppController.$inject = ["$http", "$scope"];;
+    }
+    TrafficReportAppController.$inject = ["$http", "$scope"];
+})();
 angular.module('TrafficMenuComponent', [])
     .directive('trafficMenu', function () {
         return {
